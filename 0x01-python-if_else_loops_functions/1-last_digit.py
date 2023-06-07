943abd05544516428c8a2ecb5b9595c5f1477496
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-06-07 17:56:59 +0000

    Remove 'is' comparison and use '=='

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-
-if number < 0:
-    remainder = number % (-10)
-else:
-    remainder = number % 10
-
-if remainder > 5:
+last_digit = int(repr(number)[-1])
+if last_digit > 5:
     print("Last digit of {} is {} and is greater than 5"
-          .format(number, remainder))
-elif remainder is 0:
+          .format(number, last_digit))
+elif last_digit == 0:
     print("Last digit of {} is {} and is 0"
-          .format(number, remainder))
-else:
+          .format(number, last_digit))
+elif last_digit < 6:
     print("Last digit of {} is {} and is less than 6 and not 0"
-          .format(number, remainder))
+          .format(number, last_digit))
+else:
+    pass
