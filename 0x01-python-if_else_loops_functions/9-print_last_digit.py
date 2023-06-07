3a1e957644dc297d900296a1992e4716e6f01d54
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-06-07 02:56:27 +0000

    Print the last digit of a number

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-def uppercase(str):
-    for c in str:
-        if (ord(c) >= ord('a')) and (ord(c) <= ord('z')):
-            c = chr(ord(c)-ord('a')+ord('A'))
-        print("{}".format(c), end='')
-    print()
+def print_last_digit(number):
+    print(abs(number) % 10, end='')
+    return(abs(number) % 10)
