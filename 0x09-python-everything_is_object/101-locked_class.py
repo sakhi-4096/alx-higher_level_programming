c5d352e71a3f1f8fb82037610fcbace58d791357
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-07-05 20:39:21 +0000

    Prevent the user from dynamically creating new instance attributes, except if attribute is first_name

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+Defines a class that restricts the creation of new instance attributes dynamically.
+Only the attribute "first_name" is allowed.
+"""
+
+class LockedClass:
+    """
+    Class that prevents the creation of new instance attributes dynamically,
+    except for the attribute "first_name".
+    """
+
+    __slots__ = ("first_name",)
