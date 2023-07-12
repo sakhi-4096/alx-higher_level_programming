7a6bd8cb5239ad89914adc2b14ffd1a7d71a40bd
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-07-12 04:50:41 +0200

    Add a class Student that defines a student with instanitation of given parameters and replaces all attributes of Student

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 """
 Module 11-student
+
 Contains class Student
 that initializes public instance attributes first_name, last_name, and age,
-and has public method to_json that retrieves its dictionary representation
+and has public method to_json that returns dictionary representation
+of requested attributes or all if none were requested
 """
 
 
 class Student():
     """
     Public Attributes:
-        first_name, last_name, age
+        first_name
+        last_name
+        age
+
     Public Methods:
         to_json: retrieves its dictionary representation
     """
@@ -22,10 +27,29 @@ class Student():
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """
         Returns dictionary description with simple data structure
         (list, dictionary, dictionary, string)
         for JSON serialization of an object
+
+        Return:
+            Only return dict of attrs given to us
+            Return entire dict if no attrs given
         """
-        return self.__dict__
+        if attrs is None:
+            return self.__dict__
+        else:
+            dic = {}
+            for att in attrs:
+                if att in self.__dict__.keys():
+                    dic[att] = self.__dict__[att]
+            return dic
+
+    def reload_from_json(self, json):
+        """
+        Return:
+            Transfer all attributes of json to self
+        """
+        for k, v in json.items():
+            setattr(self, k, v)
