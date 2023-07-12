90aeb4a51bed8e5f99df85f4975c3812f4d17eef
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-07-13 00:55:10 +0200

    Return a list of integer lists representing Pascal's triangle of size n

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -5,6 +5,7 @@ Module 12-pascal_triangle
 Contains function that returns int lists of pascal triangle of any given size
 """
 
+
 def pascal_triangle(n):
     """
     Return a list of integer lists representing Pascal's triangle of size n.
