47b3a779665660e33507b796bd67675e9a39cf2a
Sakhile L. Ndlazi <sakhilelindah@protonmail.com>
2023-08-16 23:10:05 +0200

    List all shows contained in 'hbtn_0d_tvshows' that have at least one genre linked.

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,12 @@
+-- import a SQL dump
+--      echo "CREATE DATABASE hbtn_0d_tvshows;" | mysql -uroot -p
+--      curl [link] -s | mysql -uroot -p hbtn_0d_tvshows
+--
+-- display: tv_shows.title - tv_show_genres.genre_id
+-- Sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+
+    SELECT tv_shows.title, tv_show_genres.genre_id
+      FROM tv_shows
+INNER JOIN tv_show_genres
+        ON tv_shows.id = tv_show_genres.show_id
+  ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
