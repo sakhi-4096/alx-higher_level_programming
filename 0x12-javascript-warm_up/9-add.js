45515fed9f2d9aae53ed98b0a9a3b849077b78be
Sakhile Lindah Ndlazi <sakhilelindah@protonmail.com>
2023-09-12 02:51:29 +0200

    Print addition of two integers.

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* Print thr addition of 2 integers */
+
+const a = parseInt(process.argv[2], 10);
+const b = parseInt(process.argv[3], 10);
+function add (x, y) {
+  return (x + y);
+}
+console.log(add(a, b));
