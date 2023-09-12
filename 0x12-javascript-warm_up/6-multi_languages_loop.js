76fcb6222268a211ce874f5096786ae526d83157
Sakhile Lindah Ndlazi <sakhilelindah@protonmail.com>
2023-09-12 02:49:38 +0200

    Print 3 lines, using an array of string and a loop.

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/* Print 3 lines, but by using an array of string and a loop */
+
+const lines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (const line of lines) {
+  console.log(line);
+}
