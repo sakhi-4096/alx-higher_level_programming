5b3e34b41a2e08e49a450c4ae33d83b3929ee2a0
Sakhile Lindah Ndlazi <sakhilelindah@protonmail.com>
2023-09-12 02:50:09 +0200

    Print 'x' times 'C is fun'.

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/* Print x times "C is fun" */
+
+const num = parseInt(process.argv[2], 10);
+if (isNaN(num)) {
+  console.log('Missing number of occurences');
+} else {
+  for (let i = num; i > 0; i -= 1) {
+    console.log('C is fun');
+  }
+}
