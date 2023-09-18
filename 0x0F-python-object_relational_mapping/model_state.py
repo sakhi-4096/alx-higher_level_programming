6393b3a5e71ff20bdc22aecdd8411da9f358bece
Sakhile Lindah Ndlazi <sakhilelindah@protonmail.com>
2023-09-18 03:05:23 +0200

    Add 'State' class inherits from 'Base Tips' links to the MySQL table states.

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+# Import necessary modules from SQLAlchemy
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+# Create a declarative base instance for defineing classes
+Base = declarative_base()
+
+
+class State(Base):
+    # Set the table name in the database
+    __tablename__ = 'states'
+
+    # Define the 'id' column, which is an auto-generated unique integer, primary key and can't be null
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+
+    # Define the 'name' column, which is a string with a maximum of 128 characters and can't be null
+    name = Column(String(128), nullable=False)
