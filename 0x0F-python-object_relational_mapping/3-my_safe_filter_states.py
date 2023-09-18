10149adbe154954c9310958321330a2f4f4be935
Sakhile Lindah Ndlazi <sakhilelindah@protonmail.com>
2023-09-18 05:05:08 +0200

    Display all values in the 'states' table of the 'hbtn_0e_0_usa' database.

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,43 +8,30 @@ import MySQLdb
 from sys import argv
 
 
-def search_states_safe(username, password, database_name, state_name):
-    try:
-        # Connect to the MySQL server
-        connection = MySQLdb.connect(
-            user=username,
-            password=password,
-            host='localhost',
-            port=3306,
-            database=database_name
-        )
-
-        # Create a cursor to interact with the database
-        cursor = connection.cursor()
-
-        # Prepare the SQL query with the parameterized query
-        query = "SELECT * FROM states WHERE name = %s ORDER BY states.id"
-
-        # Execute the parameterized query with the user input
-        cursor.execute(query, (state_name,))
-
-        # Fetch all the rpws and display them
-        states = cursor.fetchall()
-        for state in states:
-            print(state)
-
-    except MySQLdb.Error as e:
-        print("MySQL Error:", e)
-    finally:
-        # Close the cursor and connection
-        cursor.close()
-        connection.close()
-
-
 if __name__ == "__main__":
-    if len(argv) != 5:
-        print("Usage: python script.py <username> <password> <database_name> <state_name>")
-        sys.exit(1)
+    # Connect to the MySQL server
+    connection = MySQLdb.connect(
+        user=argv[1],
+        passwd=argv[2],
+        host='localhost',
+        port=3306,
+        db=argv[3]
+    )
 
-    username, password, database_name, state_name = argv[1], argv[2], argv[3], argv[4]
-    search_states_safe(username, password, database_name, state_name)
+    # Create a cursor to interact with the database
+    cursor = connection.cursor()
+
+    # Prepare the SQL query with the parameterized query
+    query = "SELECT * FROM states WHERE name = %s ORDER BY states.id"
+
+    # Execute the parameterized query with the user input
+    cursor.execute(query, (argv[4],))
+
+    # Fetch all the rpws and display them
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+
+    # Close the cursor and connection
+    cursor.close()
+    connection.close()
