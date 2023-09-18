dc9a3330f0e20fff7e1835d378d82281acb77435
Sakhile Lindah Ndlazi <sakhilelindah@protonmail.com>
2023-09-18 04:23:19 +0200

    [REFACTOR]: Make script executable.

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,5 +1,10 @@
+#!/usr/bin/python3
+"""
+Define class 'City'
+"""
+
 from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base
+from model_state import Base, State
 
 
 class City(Base):
@@ -9,6 +14,6 @@ class City(Base):
     """
     __tablename__ = 'cities'
 
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey(State.id), nullable=False)
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python3
+"""
+Define class 'State'
+"""
+
 # Import necessary modules from SQLAlchemy
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -11,7 +16,7 @@ class State(Base):
     __tablename__ = 'states'
 
     # Define the 'id' column, which is an auto-generated unique integer, primary key and can't be null
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, primary_key=True, nullable=False)
 
     # Define the 'name' column, which is a string with a maximum of 128 characters and can't be null
     name = Column(String(128), nullable=False)
