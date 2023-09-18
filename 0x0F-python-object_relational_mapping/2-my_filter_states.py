f5beada0ebc36996defd373cc20fe113038152b2
Sakhile Lindah Ndlazi <sakhilelindah@protonmail.com>
2023-09-18 03:56:38 +0200

    Fix missing comma after host when attempting MySQL server connection.

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,38 +5,32 @@ A script that takes an argument and displays all values in the
 matches the provided argument.
 """
 import MySQLdb
-import sys
+from sys import argv
 
 
-def search_states(username, password, database_name, state_name):
-    # Connect to the MySQL server
-    connection = MySQLdb.connect(
-        user=username,
-        passwd=password,
-        host='localhost',
+if __name__ == "__main__":
+    # Connect to the database
+    db_connection = MySQLdb.connect(
+        host="localhost",
         port=3306,
-        db=database_name
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
     )
 
-    # Create a cursor object to interact with the database
-    cursor = connection.cursor()
+    # Create a cursor to execute queries using SQL; match argument given
+    db_cursor = db_connection.cursor()
 
     # Prepare the SQL query with the user input and execute it
     query = """SELECT * FROM states
                 WHERE name LIKE '{:s}' ORDER BY id ASC""".format(argv[4])
-    cursor.execute(query)
+    db_cursor.execute(query)
 
-    # Fetch all the rows and display them
-    states = cursor.fetchall()
-    for state in states:
-        print(state)
+    # Fetch and print matching rows
+    for row in db_cursor.fetchall():
+        if row[1] == state_name:
+            print(row)
 
-    # Close the cursor and connection
-    cursor.close()
-    connection.close()
-
-
-if __name__ == "__main__":
-    username, password, database_name, state_name = sys.argv[
-        1], sys.argv[2], sys.argv[3], sys.argv[4]
-    search_states(username, password, database_name, state_name)
+    # Close the cursor and database connection
+    db_cursor.close()
+    db_connection.close()
