53ba801e4c102fe2d83218676626ab690bdc36d9
Sakhile Lindah Ndlazi <sakhilelindah@protonmail.com>
2023-09-18 04:56:40 +0200

    Add module defines the 'State' class, representing the states table in the database.

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This module defines the State class, representing the states table in the database.
+"""
+
+from sqlalchemy import Column, Integer, String, text
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    Represents a state in the database.
+
+    Attributes:
+        id (int): The primary key for the state.
+        name (str): The name of the state.
+        cities (relationship): A relationship to City objects associated with
+        this state.
+    """
+
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+
+    cities = relationship("City", backref="state", cascade="all, delete")
