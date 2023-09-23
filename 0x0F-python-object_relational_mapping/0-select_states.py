46dc6748cd794ce72e94f48a24d874ca3a90f8a9
Sakhile Lindah Ndlazi <sakhilelindah@protonmail.com>
2023-09-23 04:44:16 +0200

    List all states from the hbtn_0e_0_usa database using the MySQLdb module.

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,25 +7,30 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    # connect to the MySQL server
-    connection = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3]
-    )
+    try:
+        # connect to the MySQL server
+        connection = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+        )
 
-    # create a cursor object to interact with the database
-    cursor = connection.cursor()
-    # execute the SQL query to retreive states sorted by states.id
-    cursor.execute("SELCT * FROM states ORDER BY id ASC")
+        # create a cursor object to interact with the database
+        cursor = connection.cursor()
+        # execute the SQL query to retreive states sorted by states.id
+        cursor.execute("SELCT * FROM states ORDER BY id ASC")
 
-    # fetch all the rows and display them
-    states = cursor.fetchall()
-    for state in states:
-        print(state)
+        # fetch all the rows and display them
+        states = cursor.fetchall()
+        for state in states:
+            print(state)
 
-    # Close the cursor and connection
-    cursor.close()
-    connection.close()
+    except MySQLdb.Error as e:
+        print("MySQL Error:", e)
+        exit(1)
+    finally:
+        # close the cursor and connection
+        cursor.close()
+        connection.close()
