e150048bdf389543ea3cdaeda2d67f3a1a68724a
Sakhile Lindah Ndlazi <sakhilelindah@protonmail.com>
2023-09-30 09:43:07 +0200

    [REQUIREMENT]: All your Bash scripts should be exactly 3 lines long.

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# display size of body of the response
-# ./0-body_size.sh 0.0.0.0:5000
-
+# display size of body of the response, Usage: ./0-body_size.sh 0.0.0.0:5000
 curl -sI "$1" | grep -i Content-Length | cut -d' ' -f2
