83f59bae662d088ea05dbf0bb155e60c521a6f4c
Sakhile Lindah Ndlazi <sakhilelindah@protonmail.com>
2023-10-02 23:32:50 +0200

    Fix the required output format.

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,8 +9,9 @@ if __name__ == "__main__":
     # use a with statement to open the URL
     with urllib.request.urlopen(request) as response:
         # read the response body
-        body = response.read().decode('utf-8')
+        body = response.read()
         # display the response in the required format
         print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
