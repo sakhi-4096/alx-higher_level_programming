e93da8eaed948cfa5f00bf6f91a7dd414d6ca1fe
Sakhile Lindah Ndlazi <sakhilelindah@protonmail.com>
2023-10-02 23:46:52 +0200

    Send a POST request to URL with a given letter.

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,17 +7,27 @@ from sys import argv
 import requests
 
 if __name__ == "__main__":
-    search_letter = argv[1] if len(argv) > 1 else ""
+    # check if a letter argument is provided, default to an empty string if not
+    letter = argv[1] if len(argv) > 1 else ""
+    # define the URL for the POST request
     search_url = 'http://0.0.0.0:5000/search_user'
-    payload = {'q': search_letter}
-    response = requests.post(search_url, data=payload)
+    # create a payload dictionary with the letter
+    request_payload = {'q': letter}
+    # send the POST request
+    response = requests.post(search_url, data=request_payload)
 
     try:
-        response_json = response.json()
-        if response_json:
-            print("[{}] {}".format(response_json.get(
-                'id'), response_json.get('name')))
+        # parse the JSON response
+        response_data = response.json()
+
+        if response_data:
+            # display user information if available
+            user_id = response_data.get('id')
+            user_name = response_data.get('name')
+            print("[{}] {}".format(user_id, user_name))
         else:
+            # display "No result" if no user information is available
             print("No result")
     except ValueError:
+        # display "Not a valid JSON" for JSON parsing errors
         print("Not a valid JSON")
