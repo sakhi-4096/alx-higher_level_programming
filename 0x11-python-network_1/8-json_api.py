b7fb9727c82a6b754ca129349d762e6db3d86e25
Sakhile Lindah Ndlazi <sakhilelindah@protonmail.com>
2023-10-02 23:42:44 +0200

    Send a POST request to URL with a given letter.

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,31 +1,23 @@
 #!/usr/bin/python3
 """
-Python script that takes a letter as input, sends a POST request
-to the specified URL with the letter as a parameter, and processes
-the response according to the specified criteria.
+Sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
+Usage: ./8-json_api.py [letter only]
 """
 from sys import argv
 import requests
 
-
 if __name__ == "__main__":
-    # get the letter from the command-line argument, default to empty
-    # string if not provided
-    letter = argv[1] if len(argv) > 1 else ""
+    search_letter = argv[1] if len(argv) > 1 else ""
+    search_url = 'http://0.0.0.0:5000/search_user'
+    payload = {'q': search_letter}
+    response = requests.post(search_url, data=payload)
 
-    # create a dictionary with the letter as a parameter
-    data = {'q': letter}
-
-    response = requests.post("http://0.0.0.0:5000/search_user", data=data)
-
-    # check if the response has a valid JSON format and is not empty
-    if (response.headers.get('content-type') == 'application/json' and
-            response.text.strip()):
-        # check if 'id' and 'name' keys are present in the JSON
-        if 'id' in json_data and 'name' in json_data:
-            print("[{}] {}".format(json_data['id'], json_data['name']))
+    try:
+        response_json = response.json()
+        if response_json:
+            print("[{}] {}".format(response_json.get(
+                'id'), response_json.get('name')))
         else:
             print("No result")
-
-    else:
+    except ValueError:
         print("Not a valid JSON")
